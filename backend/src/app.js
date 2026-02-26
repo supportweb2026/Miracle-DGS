35a6eb79ccf05b46f554b10b66108aa7630b3035
Sodepsi Ad <dev@sodepsi.com>
2026-02-26 16:05:19 +0100

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -27,7 +27,7 @@ app.use((req, res, next) => {
 // CORS configuration
 //'https://www.crmiracle.net', 'http://localhost:3000'
 const corsOptions = {
-  origin: [*],
+  origin: ['https://www.crmiracle.net', 'http://localhost:3000'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin'],
