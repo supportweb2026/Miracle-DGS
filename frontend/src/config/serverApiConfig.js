3e909ac0ee50f4373503f0281cf604bd664427e8
Sodepsi Ad <dev@sodepsi.com>
2026-03-05 11:54:01 +0000

    ok

--- frontend/src/config/serverApiConfig.js
+++ frontend/src/config/serverApiConfig.js
@@ -1,18 +1,18 @@
 export const API_BASE_URL =
   import.meta.env.PROD || import.meta.env.VITE_DEV_REMOTE == 'remote'
-    ? 'https://web-production-2e766.up.railway.app/api/'
+    ? 'https://miracle-dgs.onrender.com/api/'
     : 'http://localhost:8888/api/';
 export const BASE_URL =
   import.meta.env.PROD || import.meta.env.VITE_DEV_REMOTE
-    ? 'https://web-production-2e766.up.railway.app/'
+    ? 'https://https://miracle-dgs.onrender.com/'
     : 'http://localhost:8888/';
 
 export const WEBSITE_URL = import.meta.env.PROD
-  ? 'https://web-production-2e766.up.railway.app/'
+  ? 'https://https://miracle-dgs.onrender.com/'
   : 'http://localhost:3000/';
 export const DOWNLOAD_BASE_URL =
   import.meta.env.PROD || import.meta.env.VITE_DEV_REMOTE
-    ? 'https://web-production-2e766.up.railway.app/download/'
+    ? 'https://https://miracle-dgs.onrender.com/download/'
     : 'http://localhost:8888/download/';
 export const ACCESS_TOKEN_NAME = 'x-auth-token';
 
